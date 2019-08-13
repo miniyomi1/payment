5d46cda991cfa95281286544528c388c25c5f99d
Lacie Chen <miniyomi1@gmail.com>
2019-08-13 16:27:09 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,1 +1,32 @@
+let paymentType = 'credit';
 
+function changePaymentInfo(){
+    $('.payment-btn').removeClass('active');
+    $(this).addClass('active');
+    $(this).hasClass('cridit-btn') ? (
+        $('.payment-info-panel').removeClass('active'), $(".credit-card").addClass('active')) :
+        ($(this).hasClass('market-btn') ? (
+            $('.payment-info-panel').removeClass('active'), $(".convenience-store").addClass('active')) :
+            ($(this).hasClass('atm-btn') ? (
+                $(".payment-info-panel").removeClass('active'), $('.atm').addClass('active')) :(alert('錯誤'))));
+}
+
+function paymentSubmit(a) {
+    $(".payment-info>.active>form").submit()
+}
+
+$(document).ready(function () {
+    $('.payment-btn').on('click', changePaymentInfo);
+    // $('input').on('keyup',checkEmail);
+    $('.submit-btn').on('click',paymentSubmit);
+});
+
+window.Parsley.addValidator("dkPhone", {
+    requirementType: "integer",
+    validateNumber: function (value) {
+        return value.toString().length === 8;
+    },
+    messages: {
+        en: "This value should be valid Danish phone number"
+    }
+});
